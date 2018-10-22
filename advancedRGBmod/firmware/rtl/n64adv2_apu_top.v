bb0bbf959f913138b95af1c879165fa022f9dba1
borti4938 <borti4938@gmx.de>
2018-10-22 20:20:05 +0200

    introducing a muxed FIR filter as well as alternative filter coefficients to use

--- advancedRGBmod/firmware/rtl/n64adv2_apu_top.v
+++ advancedRGBmod/firmware/rtl/n64adv2_apu_top.v
@@ -36,7 +36,7 @@
 //
 //////////////////////////////////////////////////////////////////////////////////
 
-
+`define MUXED_FIR // using muxed FIR filter increases LE load but uses one M9K less then two dedicated FIRs
 
 
 module n64adv2_apu_top (
@@ -85,28 +85,112 @@ n64_sample_i2s i2s_rx_u(
 
 // interpolation
 
-wire signed [23:0] APDATA_INT [0:1];
-wire [1:0] APDATA_INT_VALID;
+`ifndef MUXED_FIR
 
-fir_audio audio_l_u(
-  .clk(AMCLK_i),
-  .reset_n(nARST),
-  .ast_sink_data(APDATA[1]),
-  .ast_sink_valid(APDATA_VALID),
-  .ast_sink_error(2'b00),
-  .ast_source_data(APDATA_INT[1]),
-  .ast_source_valid(APDATA_INT_VALID[1])
-);
+  wire signed [23:0] APDATA_INT [0:1];
+  wire [1:0] APDATA_INT_VALID;
 
-fir_audio audio_r_u(
-  .clk(AMCLK_i),
-  .reset_n(nARST),
-  .ast_sink_data(APDATA[0]),
-  .ast_sink_valid(APDATA_VALID),
-  .ast_sink_error(2'b00),
-  .ast_source_data(APDATA_INT[0]),
-  .ast_source_valid(APDATA_INT_VALID[0])
-);
+  fir_audio fir_audio_l_u(
+    .clk(AMCLK_i),
+    .reset_n(nARST),
+    .ast_sink_data(APDATA[1]),
+    .ast_sink_valid(APDATA_VALID),
+    .ast_sink_error(2'b00),
+    .ast_source_data(APDATA_INT[1]),
+    .ast_source_valid(APDATA_INT_VALID[1])
+  );
+
+  fir_audio fir_audio_r_u(
+    .clk(AMCLK_i),
+    .reset_n(nARST),
+    .ast_sink_data(APDATA[0]),
+    .ast_sink_valid(APDATA_VALID),
+    .ast_sink_error(2'b00),
+    .ast_source_data(APDATA_INT[0]),
+    .ast_source_valid(APDATA_INT_VALID[0])
+  );
+
+`else
+
+  reg [1:0] tdm = 2'b11;
+  reg [15:0] sink_data_buf_0, sink_data_buf_1, sink_data;
+  reg sink_valid, sink_sop, sink_eop;
+
+  always @(posedge AMCLK_i) begin
+    case (tdm)
+      2'b00: if (APDATA_VALID) begin
+        sink_data_buf_1 <= APDATA[1];
+        sink_data_buf_0 <= APDATA[0];
+        tdm <= 2'b01;
+      end
+      2'b01: begin
+        sink_data <= sink_data_buf_1;
+        sink_valid <= 1'b1;
+        sink_sop <= 1'b1;
+        sink_eop <= 1'b0;
+        tdm <= 2'b10;
+      end
+      2'b10: begin
+        sink_data <= sink_data_buf_0;
+        sink_valid <= 1'b1;
+        sink_sop <= 1'b0;
+        sink_eop <= 1'b1;
+        tdm <= 2'b11;
+      end
+      2'b11: begin
+        sink_valid <= 1'b0;
+        sink_sop <= 1'b0;
+        sink_eop <= 1'b0;
+        tdm <= 2'b00;
+      end
+      default: tdm <= 2'b11;
+    endcase
+    if (!nARST) begin
+      sink_valid <= 1'b0;
+      sink_sop <= 1'b0;
+      sink_eop <= 1'b0;
+      tdm <= 2'b00;
+    end
+  end
+
+  wire [23:0] source_data;
+  wire source_valid, source_sop, source_eop, source_channel;
+
+  fir_2ch_audio fir_2ch_audio_u(
+    .clk(AMCLK_i),
+    .reset_n(nARST),
+    .ast_sink_data(sink_data),
+    .ast_sink_valid(sink_valid),
+    .ast_sink_error(2'b00),
+    .ast_sink_sop(sink_sop),
+    .ast_sink_eop(sink_eop),
+    .ast_source_data(source_data),
+    .ast_source_valid(source_valid),
+    .ast_source_sop(source_sop),
+    .ast_source_eop(source_eop),
+    .ast_source_channel(source_channel)
+  );
+
+
+  reg signed [23:0] APDATA_INT [0:1];
+  reg [1:0] APDATA_INT_VALID;
+
+  always @(posedge AMCLK_i) begin
+    if (source_valid) begin
+      APDATA_INT[source_sop] <= source_data;
+      APDATA_INT_VALID[source_sop] <= 1'b1;
+      APDATA_INT_VALID[source_eop] <= 1'b0;
+    end else begin
+      APDATA_INT_VALID <= 2'b00;
+    end
+    if (!nARST) begin
+      APDATA_INT[0] <= {24{1'b0}};
+      APDATA_INT[1] <= {24{1'b0}};
+      APDATA_INT_VALID <= 2'b00;
+    end
+  end
+
+`endif
 
 
 // seriellization
