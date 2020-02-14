081cc64c4f254f904826f50817ddad5c9f56f5b6
borti4938 <borti4938@gmail.com>
2020-02-14 16:38:10 +0100

    fix header quirk in mfiles

--- advancedRGBmod/firmware/scripts/font2hex.m
+++ advancedRGBmod/firmware/scripts/font2hex.m
@@ -1,21 +1,21 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %
-% 'This 'file 'is 'part 'of 'the 'N64 'RGB/YPbPr 'DAC 'project.
+% This file is part of the N64 RGB/YPbPr DAC project.
 %
-% 'Copyright '(C) '2015-2020 'by 'Peter 'Bartmann '<borti4938@gmail.com>
+% Copyright (C) 2015-2020 by Peter Bartmann <borti4938@gmail.com>
 %
-% 'N64 'RGB/YPbPr 'DAC 'is 'free 'software: 'you 'can 'redistribute 'it 'and/or 'modify
-% 'it 'under 'the 'terms 'of 'the 'GNU 'General 'Public 'License 'as 'published 'by
-% 'the 'Free 'Software 'Foundation';' 'either 'version '3 'of 'the 'License';' 'or
-% 'any 'later 'version.
+% N64 RGB/YPbPr DAC is free software: you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation, either version 3 of the License, or
+% any later version.
 %
-% 'This 'program 'is 'distributed 'in 'the 'hope 'that 'it 'will 'be 'useful';'
-% 'but 'WITHOUT 'ANY 'WARRANTY';without 'even 'the 'implied 'warranty 'of
-% 'MERCHANTABILITY 'or 'FITNESS 'FOR 'A 'PARTICULAR 'PURPOSE. 'See 'the
-% 'GNU 'General 'Public 'License 'for 'more 'details.
+% This program is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU General Public License for more details.
 %
-% 'You 'should 'have 'received 'a 'copy 'of 'the 'GNU 'General 'Public 'License
-% 'along 'with 'this 'program. 'If 'not';' 'see '<http:%www.gnu.org/licenses/>.
+% You should have received a copy of the GNU General Public License
+% along with this program.  If not, see <http:/www.gnu.org/licenses/>.
 %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
