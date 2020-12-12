600c9b18c0ed6a8653ff034f0ae47ab1447512c4
borti4938 <borti4938@gmail.com>
2020-12-12 23:12:10 +0100

    put wire and reg defs to top of file

--- advancedRGBmod/firmware/rtl/ppu/linemult.v
+++ advancedRGBmod/firmware/rtl/ppu/linemult.v
@@ -47,11 +47,6 @@ module linemult(
 
 `include "vh/n64adv_vparams.vh"
 
-localparam hcnt_width  = $clog2(`PIXEL_PER_LINE_MAX_2x);
-localparam Y_width     = color_width_o+1;
-localparam SLHyb_width = 8; // do not change this localparam!
-localparam pcnt_width = $clog2(`BUF_NUM_OF_PAGES);
-
 input VCLK_i;
 input nVRST_i;
 input vdata_valid_i; // for new implementation
@@ -62,18 +57,19 @@ input [20:0] vinfo_mult; // [nLineMult (2bits),lx_ifix (1bit),SLhyb_str (5bits),
 input  VCLK_o;
 input  nVRST_o;
 output reg vdata_valid_o = 1'b1; // for new implementation
-output [`VDATA_O_FU_SLICE] vdata_o;
+output reg [`VDATA_O_FU_SLICE] vdata_o = {vdata_width_o{1'b0}};
 
 
-// pre-assignments
+// mist stuff
+localparam hcnt_width  = $clog2(`PIXEL_PER_LINE_MAX_2x);
+localparam pcnt_width = $clog2(`BUF_NUM_OF_PAGES);
+localparam Y_width     = color_width_o+1;
+localparam SLHyb_width = 8; // do not change this localparam!
 
-wire nVS_i = vdata_i[3*color_width_i+3];
-wire nHS_i = vdata_i[3*color_width_i+1];
+integer int_idx, pp_idx;
 
-wire [color_width_i-1:0] R_i = vdata_i[`VDATA_I_RE_SLICE];
-wire [color_width_i-1:0] G_i = vdata_i[`VDATA_I_GR_SLICE];
-wire [color_width_i-1:0] B_i = vdata_i[`VDATA_I_BL_SLICE];
 
+// configuration
 wire   nENABLE_linemult = ~^vinfo_mult[20:19];
 wire [1:0] linemult_sel =   vinfo_mult[20:19]; // LineX3 not allowed in PAL mode (covered in upper module)
 wire       bob_480i_fix =   vinfo_mult[18];
@@ -84,20 +80,149 @@ wire              SL_id =   vinfo_mult[ 3];
 wire              SL_en =   vinfo_mult[ 2];
 wire            palmode =   vinfo_mult[ 1];
 wire           n64_480i =   vinfo_mult[ 0];
- 
+
+// wires
+wire nVS_i, nHS_i;
+wire negedge_nHSYNC, negedge_nVSYNC;
+
+wire line_overflow, valid_line;
+
+wire [pcnt_width-1:0] wrpage_o_resynced;
+wire wrhcnt_b6_o_resynced, valid_line_r_o_resynced, start_rdproc_o_resynced;
+
+
+wire [          10:0] vpos_o;
+wire [hcnt_width-1:0] hpos_o;
+
+wire [pcnt_width-1:0] rdpage_pp0, rdpage_pp1, rdpage_pp2, rdpage_pp3;
+wire [color_width_i-1:0] R_buf, G_buf, B_buf;
+
+
+wire [color_width_i-1:0] R_avg, G_avg, B_avg;
+
+
+wire [Y_width+4:0] Y_ref_pre_full;
+wire [Y_width+SLHyb_width-1:0] Y_ref_full;
+wire [color_width_o+SLHyb_width-2:0] R_sl_full, G_sl_full, B_sl_full;
+
+
+// regs
+reg nVDSYNC_dbl = 1'b0;
+reg nVS_i_buf = 1'b0;
+reg nHS_i_buf = 1'b0;
+reg FrameID = 1'b0;
+
+
+reg [hcnt_width-1:0] hstart_i = `HSTART_NTSC_2x;
+reg [hcnt_width-1:0] hstop_i  = `HSTOP_NTSC_2x;
+
+reg                  wren   = 1'b0;
+reg [pcnt_width-1:0] wrpage = {pcnt_width{1'b0}};
+reg [hcnt_width-1:0] wrhcnt = {hcnt_width{1'b1}};
+reg [hcnt_width-1:0] wraddr = {hcnt_width{1'b0}};
+
+reg line_overflow_r = 1'b0;
+reg valid_line_r    = 1'b0;
+
+reg [hcnt_width-1:0] line_width[0:`BUF_NUM_OF_PAGES-1];
+initial begin
+  for (int_idx = 0; int_idx < `BUF_NUM_OF_PAGES; int_idx = int_idx+1)
+    line_width[int_idx] = {hcnt_width{1'b0}};
+end
+
+
+reg [1:0] newFrame = 2'b0; // newFrame[1] used by reading process, newFrame[0] by writing process
+reg start_rdproc = 1'b0;
+
+
+reg [hcnt_width-1:0] hstart_o = `HSTART_NTSC_2x;
+reg [hcnt_width-1:0] hstop_o  = `HSTOP_NTSC_2x;
+
+reg [ 6:0] nHS_width = `HS_WIDTH_NTSC_LX2_2x;
+reg [ 4:0] pic_shift = `H_SHIFT_NTSC_240P_LX2_2x;
+reg [ 3:0] nVS_width = `VS_WIDTH_NTSC_LX2;
+reg [10:0] nVS_delay = (`BUF_NUM_OF_PAGES>>1);
+
+reg            [2:0] rden    = 3'b0;
+reg            [1:0] rdrun   = 2'b00;
+reg            [1:0] rdcnt   = 2'b0;
+reg [pcnt_width-1:0] rdpage  = {pcnt_width{1'b0}};
+reg [hcnt_width-1:0] rdhcnt  = {hcnt_width{1'b0}};
+reg [          10:0] rdvcnt  = 11'd0;
+reg [hcnt_width-1:0] rdaddr  = {hcnt_width{1'b0}};
+
+reg start_rdproc_o_resynced_pre = 1'b0;
+
+reg nHSYNC_mult = 1'b0;
+reg nVSYNC_mult = 1'b0;
+reg nCSYNC_mult = 1'b0;
+
+
+reg                     drawSL [0:2];
+reg               [3:0] S_mult [0:2];
+reg [color_width_i-1:0] R_mult_pre = {color_width_i{1'b0}};
+reg [color_width_i-1:0] G_mult_pre = {color_width_i{1'b0}};
+reg [color_width_i-1:0] B_mult_pre = {color_width_i{1'b0}};
+reg [color_width_i-1:0] R_sl_pre = {color_width_i{1'b0}};
+reg [color_width_i-1:0] G_sl_pre = {color_width_i{1'b0}};
+reg [color_width_i-1:0] B_sl_pre = {color_width_i{1'b0}};
+reg [color_width_i-1:0] R_mult = {color_width_i{1'b0}};
+reg [color_width_i-1:0] G_mult = {color_width_i{1'b0}};
+reg [color_width_i-1:0] B_mult = {color_width_i{1'b0}};
+reg [Y_width-1:0] Y_sl_pre = {Y_width{1'b0}};
+
+initial begin
+  for (int_idx = 0; int_idx < 3; int_idx = int_idx+1) begin
+    drawSL[int_idx] = 1'b0;
+    S_mult[int_idx] = 4'b0000;
+  end
+end
+
+
+reg                     dSL_pp[0:4]                     /* synthesis ramstyle = "logic" */;
+reg               [3:0] S_pp[0:4]                       /* synthesis ramstyle = "logic" */;
+reg [color_width_i-1:0] R_sl_pre_pp[0:3],
+                        G_sl_pre_pp[0:3],
+                        B_sl_pre_pp[0:3]                /* synthesis ramstyle = "logic" */;
+reg [color_width_i-1:0] R_pp[0:4], G_pp[0:4], B_pp[0:4] /* synthesis ramstyle = "logic" */;
+
+initial begin
+  for (int_idx = 0; int_idx < 5; int_idx = int_idx+1) begin
+    dSL_pp[int_idx] <= 1'b0;
+    S_pp[int_idx] <= 4'b0000;
+    R_pp[int_idx] <= {color_width_i{1'b0}};
+    G_pp[int_idx] <= {color_width_i{1'b0}};
+    B_pp[int_idx] <= {color_width_i{1'b0}};
+  end
+  for (int_idx = 0; int_idx < 4; int_idx = int_idx+1) begin
+    R_sl_pre_pp[int_idx] <= {color_width_i{1'b0}};
+    G_sl_pre_pp[int_idx] <= {color_width_i{1'b0}};
+    B_sl_pre_pp[int_idx] <= {color_width_i{1'b0}};
+  end
+end
+
+reg [Y_width-1:0] Y_ref_pre = {Y_width{1'b0}};
+
+reg [Y_width-1:0] Y_ref = {Y_width{1'b0}};
+
+reg [SLHyb_width-1:0] SLHyb_rval = {SLHyb_width{1'b0}};
+reg [SLHyb_width-1:0] SLHyb_str = {SLHyb_width{1'b0}};
+
+reg [color_width_o-1:0] R_sl, G_sl, B_sl;
+initial begin
+  R_sl <= {color_width_o{1'b0}};
+  G_sl <= {color_width_o{1'b0}};
+  B_sl <= {color_width_o{1'b0}};
+end
+
+
 
 // start of rtl
+assign nVS_i = vdata_i[3*color_width_i+3];
+assign nHS_i = vdata_i[3*color_width_i+1];
+assign negedge_nHSYNC =  nHS_i_buf & !nHS_i;
+assign negedge_nVSYNC =  nVS_i_buf & !nVS_i;
 
-reg nVDSYNC_dbl = 1'b0;
-
-reg  nVS_i_buf = 1'b0;
-reg  nHS_i_buf = 1'b0;
-
-wire negedge_nHSYNC =  nHS_i_buf & !nHS_i;
-//wire posedge_nHSYNC = !nHS_i_buf &  nHS_i;
-wire negedge_nVSYNC =  nVS_i_buf & !nVS_i;
-
-reg  FrameID = 1'b0;
 
 always @(posedge VCLK_i or negedge nVRST_i)
   if (!nVRST_i) begin
@@ -119,31 +244,9 @@ always @(posedge VCLK_i or negedge nVRST_i)
   end
 
 
-reg [hcnt_width-1:0] hstart_i = `HSTART_NTSC_2x;
-reg [hcnt_width-1:0] hstop_i  = `HSTOP_NTSC_2x;
 
-reg                  wren   = 1'b0;
-reg [pcnt_width-1:0] wrpage = {pcnt_width{1'b0}};
-reg [hcnt_width-1:0] wrhcnt = {hcnt_width{1'b1}};
-reg [hcnt_width-1:0] wraddr = {hcnt_width{1'b0}};
-
-wire line_overflow = wrhcnt == `PIXEL_PER_LINE_MAX_2x;  // close approach for NTSC and PAL (equals 1600)
-wire valid_line    = wrhcnt > hstop_i & !line_overflow; // for evaluation
-
-reg line_overflow_r = 1'b0;
-reg valid_line_r    = 1'b0;
-
-
-reg [hcnt_width-1:0] line_width[0:`BUF_NUM_OF_PAGES-1];
-integer int_idx;
-initial begin
-  for (int_idx = 0; int_idx < `BUF_NUM_OF_PAGES; int_idx = int_idx+1)
-    line_width[int_idx] = {hcnt_width{1'b0}};
-end
-
-reg [1:0] newFrame = 2'b0; // newFrame[1] used by reading process
-reg   start_rdproc = 1'b0;
-
+assign line_overflow = wrhcnt == `PIXEL_PER_LINE_MAX_2x;  // close approach for NTSC and PAL (equals 1600)
+assign valid_line    = wrhcnt > hstop_i & !line_overflow; // for evaluation
 
 always @(posedge VCLK_i or negedge nVRST_i)
   if (!nVRST_i) begin
@@ -204,8 +307,6 @@ always @(posedge VCLK_i or negedge nVRST_i)
   end
 
 
-wire [pcnt_width-1:0] wrpage_o_resynced;
-wire wrhcnt_b6_o_resynced, valid_line_r_o_resynced, start_rdproc_o_resynced;
 register_sync #(
   .reg_width(pcnt_width+3),
   .reg_preset({{pcnt_width{1'b0}},3'b000})
@@ -218,30 +319,9 @@ register_sync #(
 );
 
 
-reg [hcnt_width-1:0] hstart_o = `HSTART_NTSC_2x;
-reg [hcnt_width-1:0] hstop_o  = `HSTOP_NTSC_2x;
 
-reg [ 6:0] nHS_width = `HS_WIDTH_NTSC_LX2_2x;
-reg [ 4:0] pic_shift = `H_SHIFT_NTSC_240P_LX2_2x;
-reg [ 3:0] nVS_width = `VS_WIDTH_NTSC_LX2;
-reg [10:0] nVS_delay = (`BUF_NUM_OF_PAGES>>1);
-
-reg            [2:0] rden    = 3'b0;
-reg            [1:0] rdrun   = 2'b00;
-reg            [1:0] rdcnt   = 2'b0;
-reg [pcnt_width-1:0] rdpage  = {pcnt_width{1'b0}};
-reg [hcnt_width-1:0] rdhcnt  = {hcnt_width{1'b0}};
-reg [          10:0] rdvcnt  = 11'd0;
-reg [hcnt_width-1:0] rdaddr  = {hcnt_width{1'b0}};
-
-wire [          10:0] vpos_o = rdvcnt - nVS_delay;
-wire [hcnt_width-1:0] hpos_o = rdhcnt;
-
-reg start_rdproc_o_resynced_pre = 1'b0;
-
-reg nHSYNC_mult = 1'b0;
-reg nVSYNC_mult = 1'b0;
-reg nCSYNC_mult = 1'b0;
+assign vpos_o = rdvcnt - nVS_delay;
+assign hpos_o = rdhcnt;
 
 always @(posedge VCLK_o or negedge nVRST_o)
   if (!nVRST_o) begin
@@ -341,18 +421,16 @@ always @(posedge VCLK_o or negedge nVRST_o)
 
     nHSYNC_mult <= hpos_o >= nHS_width;
     nVSYNC_mult <= vpos_o >= nVS_width;
-    nCSYNC_mult <= nVSYNC_mult == 1'b1 ? hpos_o >= nHS_width : hpos_o > (line_width[rdpage_pp1] - nHS_width);
+    nCSYNC_mult <= vpos_o >= nVS_width ? hpos_o >= nHS_width : hpos_o > (line_width[rdpage_pp1] - nHS_width);
   end
 
 
-wire [pcnt_width-1:0] rdpage_pp0 = (FrameID | !n64_480i | !bob_480i_fix) ? rdpage : rdpage - !rdcnt[0];
-wire [pcnt_width-1:0] rdpage_pp1 = rdpage_pp0 >= `BUF_NUM_OF_PAGES ? `BUF_NUM_OF_PAGES-1 : rdpage_pp0;
-wire [pcnt_width-1:0] rdpage_pp2 = (!SL_method | n64_480i) ? rdpage_pp1 :  // do not allow advanced scanlines in 480i linex2 mode
-                                   !rdaddr[0] ? rdpage_pp1 : !SL_id ? rdpage_pp1 - 1'b1 : rdpage_pp1 + 1'b1;
-wire [pcnt_width-1:0] rdpage_pp3 = !SL_id ? (rdpage_pp2 >= `BUF_NUM_OF_PAGES ? `BUF_NUM_OF_PAGES-1 : rdpage_pp2) :
-                                            (rdpage_pp2 == `BUF_NUM_OF_PAGES ?  {pcnt_width{1'b0}} : rdpage_pp2);
-
-wire [color_width_i-1:0] R_buf, G_buf, B_buf;
+assign rdpage_pp0 = (FrameID | !n64_480i | !bob_480i_fix) ? rdpage : rdpage - !rdcnt[0];
+assign rdpage_pp1 = rdpage_pp0 >= `BUF_NUM_OF_PAGES ? `BUF_NUM_OF_PAGES-1 : rdpage_pp0;
+assign rdpage_pp2 = (!SL_method | n64_480i) ? rdpage_pp1 :  // do not allow advanced scanlines in 480i linex2 mode
+                                 !rdaddr[0] ? rdpage_pp1 : !SL_id ? rdpage_pp1 - 1'b1 : rdpage_pp1 + 1'b1;
+assign rdpage_pp3 = !SL_id ? (rdpage_pp2 >= `BUF_NUM_OF_PAGES ? `BUF_NUM_OF_PAGES-1 : rdpage_pp2) :
+                             (rdpage_pp2 == `BUF_NUM_OF_PAGES ?  {pcnt_width{1'b0}} : rdpage_pp2);
 
 ram2port #(
   .num_of_pages(`BUF_NUM_OF_PAGES),
@@ -363,7 +441,7 @@ ram2port #(
   .wren(&{wren,!line_overflow,nVDSYNC_dbl,!wraddr[0]}),
   .wrpage(wrpage),
   .wraddr(wraddr[hcnt_width-1:1]),
-  .wrdata({R_i,G_i,B_i}),
+  .wrdata(vdata_i[`VDATA_I_CO_SLICE]),
   .rdCLK(VCLK_o),
   .rden(rden[0]), // leave reading open even if rdaddr[0] == 1 for advanced scanline method
   .rdpage(rdpage_pp3),
@@ -372,29 +450,9 @@ ram2port #(
 );
 
 
-reg                     drawSL [0:2];
-reg               [3:0] S_mult [0:2];
-reg [color_width_i-1:0] R_mult_pre = {color_width_i{1'b0}};
-reg [color_width_i-1:0] G_mult_pre = {color_width_i{1'b0}};
-reg [color_width_i-1:0] B_mult_pre = {color_width_i{1'b0}};
-reg [color_width_i-1:0] R_sl_pre = {color_width_i{1'b0}};
-reg [color_width_i-1:0] G_sl_pre = {color_width_i{1'b0}};
-reg [color_width_i-1:0] B_sl_pre = {color_width_i{1'b0}};
-reg [color_width_i-1:0] R_mult = {color_width_i{1'b0}};
-reg [color_width_i-1:0] G_mult = {color_width_i{1'b0}};
-reg [color_width_i-1:0] B_mult = {color_width_i{1'b0}};
-reg [Y_width-1:0] Y_sl_pre = {Y_width{1'b0}};
-
-initial begin
-  for (int_idx = 0; int_idx < 3; int_idx = int_idx+1) begin
-    drawSL[int_idx] = 1'b0;
-    S_mult[int_idx] = 4'b0000;
-  end
-end
-
-wire [color_width_i-1:0] R_avg = {1'b0,R_mult_pre[color_width_i-1:1]} + {1'b0,R_buf[color_width_i-1:1]} + (R_mult_pre[0] ^ R_buf[0]);
-wire [color_width_i-1:0] G_avg = {1'b0,G_mult_pre[color_width_i-1:1]} + {1'b0,G_buf[color_width_i-1:1]} + (G_mult_pre[0] ^ G_buf[0]);
-wire [color_width_i-1:0] B_avg = {1'b0,B_mult_pre[color_width_i-1:1]} + {1'b0,B_buf[color_width_i-1:1]} + (B_mult_pre[0] ^ B_buf[0]);
+assign R_avg = {1'b0,R_mult_pre[color_width_i-1:1]} + {1'b0,R_buf[color_width_i-1:1]} + (R_mult_pre[0] ^ R_buf[0]);
+assign G_avg = {1'b0,G_mult_pre[color_width_i-1:1]} + {1'b0,G_buf[color_width_i-1:1]} + (G_mult_pre[0] ^ G_buf[0]);
+assign B_avg = {1'b0,B_mult_pre[color_width_i-1:1]} + {1'b0,B_buf[color_width_i-1:1]} + (B_mult_pre[0] ^ B_buf[0]);
 
 always @(posedge VCLK_o or negedge nVRST_o)
   if (!nVRST_o) begin
@@ -451,53 +509,11 @@ always @(posedge VCLK_o or negedge nVRST_o)
 
 
 // post-processing (scanline generation)
-
-reg                     dSL_pp[0:4]                     /* synthesis ramstyle = "logic" */;
-reg               [3:0] S_pp[0:4], S_o                  /* synthesis ramstyle = "logic" */;
-reg [color_width_i-1:0] R_sl_pre_pp[0:3],
-                        G_sl_pre_pp[0:3],
-                        B_sl_pre_pp[0:3]                /* synthesis ramstyle = "logic" */;
-reg [color_width_i-1:0] R_pp[0:4], G_pp[0:4], B_pp[0:4] /* synthesis ramstyle = "logic" */;
-reg [color_width_o-1:0] R_o, G_o, B_o                   /* synthesis ramstyle = "logic" */;
-
-initial begin
-  for (int_idx = 0; int_idx < 5; int_idx = int_idx+1) begin
-    dSL_pp[int_idx] <= 1'b0;
-    S_pp[int_idx] <= 4'b0000;
-    R_pp[int_idx] <= {color_width_i{1'b0}};
-    G_pp[int_idx] <= {color_width_i{1'b0}};
-    B_pp[int_idx] <= {color_width_i{1'b0}};
-  end
-  for (int_idx = 0; int_idx < 4; int_idx = int_idx+1) begin
-    R_sl_pre_pp[int_idx] <= {color_width_i{1'b0}};
-    G_sl_pre_pp[int_idx] <= {color_width_i{1'b0}};
-    B_sl_pre_pp[int_idx] <= {color_width_i{1'b0}};
-  end
-  R_o <= {color_width_o{1'b0}};
-  G_o <= {color_width_o{1'b0}};
-  B_o <= {color_width_o{1'b0}};
-end
-
-wire [Y_width+4:0] Y_ref_pre_full = Y_sl_pre * (* multstyle = "dsp" *) SLHyb_depth;
-reg  [Y_width-1:0] Y_ref_pre = {Y_width{1'b0}};
-
-wire [Y_width+SLHyb_width-1:0] Y_ref_full = Y_ref_pre * (* multstyle = "dsp" *) SL_str;
-reg  [Y_width-1:0] Y_ref = {Y_width{1'b0}};
-
-reg [SLHyb_width-1:0] SLHyb_rval = {SLHyb_width{1'b0}};
-reg [SLHyb_width-1:0] SLHyb_str = {SLHyb_width{1'b0}};
-
-wire [color_width_o+SLHyb_width-2:0] R_sl_full = R_sl_pre_pp[3] * (* multstyle = "dsp" *) SLHyb_str;
-wire [color_width_o+SLHyb_width-2:0] G_sl_full = G_sl_pre_pp[3] * (* multstyle = "dsp" *) SLHyb_str;
-wire [color_width_o+SLHyb_width-2:0] B_sl_full = B_sl_pre_pp[3] * (* multstyle = "dsp" *) SLHyb_str;
-reg  [color_width_o-1:0]             R_sl, G_sl, B_sl;
-initial begin
-  R_sl <= {color_width_o{1'b0}};
-  G_sl <= {color_width_o{1'b0}};
-  B_sl <= {color_width_o{1'b0}};
-end
-
-integer pp_idx;
+assign Y_ref_pre_full = Y_sl_pre * (* multstyle = "dsp" *) SLHyb_depth;
+assign Y_ref_full = Y_ref_pre * (* multstyle = "dsp" *) SL_str;
+assign R_sl_full = R_sl_pre_pp[3] * (* multstyle = "dsp" *) SLHyb_str;
+assign G_sl_full = G_sl_pre_pp[3] * (* multstyle = "dsp" *) SLHyb_str;
+assign B_sl_full = B_sl_pre_pp[3] * (* multstyle = "dsp" *) SLHyb_str;
 
 always @(posedge VCLK_o or negedge nVRST_o)
   if (!nVRST_o) begin
@@ -513,10 +529,6 @@ always @(posedge VCLK_o or negedge nVRST_o)
       G_sl_pre_pp[int_idx] <= {color_width_i{1'b0}};
       B_sl_pre_pp[int_idx] <= {color_width_i{1'b0}};
     end
-    R_o <= {color_width_o{1'b0}};
-    G_o <= {color_width_o{1'b0}};
-    B_o <= {color_width_o{1'b0}};
-
     Y_ref_pre <= {Y_width{1'b0}};
     Y_ref <= {Y_width{1'b0}};
 
@@ -526,6 +538,8 @@ always @(posedge VCLK_o or negedge nVRST_o)
     R_sl <= {color_width_o{1'b0}};
     G_sl <= {color_width_o{1'b0}};
     B_sl <= {color_width_o{1'b0}};
+
+    vdata_o <= {vdata_width_o{1'b0}};
   end else begin
          dSL_pp[0] <= drawSL[2];
            S_pp[0] <= S_mult[2];
@@ -550,43 +564,37 @@ always @(posedge VCLK_o or negedge nVRST_o)
           R_pp[4] <=       R_pp[3];
           G_pp[4] <=       G_pp[3];
           B_pp[4] <=       B_pp[3];
-           S_o    <=       S_pp[4];
+
 
     // hybrid strength reference (2 pp stages)
-    Y_ref_pre <= Y_ref_pre_full[Y_width+4:5];
-    Y_ref     <= Y_ref_full[Y_width+SLHyb_width-1:SLHyb_width];
+    Y_ref_pre <= Y_ref_pre_full[Y_width+4:5];                   // stage [0]
+    Y_ref     <= Y_ref_full[Y_width+SLHyb_width-1:SLHyb_width]; // stage [1]
 
     // adaptation of sl_str. (2 pp stages)
-    SLHyb_rval <= {1'b0,SL_str} < Y_ref ? 8'h0 : SL_str - Y_ref[7:0];
-    SLHyb_str  <= 8'hff - SLHyb_rval;
+    SLHyb_rval <= {1'b0,SL_str} < Y_ref ? 8'h0 : SL_str - Y_ref[7:0]; // stage [2]
+    SLHyb_str  <= 8'hff - SLHyb_rval;                                 // stage [3]
     
     // calculate SL (1 pp stage)
-    R_sl <= R_sl_full[color_width_o+SLHyb_width-2:SLHyb_width-1];
-    G_sl <= G_sl_full[color_width_o+SLHyb_width-2:SLHyb_width-1];
-    B_sl <= B_sl_full[color_width_o+SLHyb_width-2:SLHyb_width-1];
+    R_sl <= R_sl_full[color_width_o+SLHyb_width-2:SLHyb_width-1]; // stage [4]
+    G_sl <= G_sl_full[color_width_o+SLHyb_width-2:SLHyb_width-1]; // stage [4]
+    B_sl <= B_sl_full[color_width_o+SLHyb_width-2:SLHyb_width-1]; // stage [4]
 
-    // set scanline
-    if (dSL_pp[4]) begin
-      R_o <= R_sl;
-      G_o <= G_sl;
-      B_o <= B_sl;
-    end else begin
-      R_o <= {R_pp[4],R_pp[4][color_width_i-1]};
-      G_o <= {G_pp[4],G_pp[4][color_width_i-1]};
-      B_o <= {B_pp[4],B_pp[4][color_width_i-1]};
-    end
+    // set output
+    vdata_o[`VDATA_O_SY_SLICE] <= S_pp[4];
+    if (dSL_pp[4])  // scanline
+      vdata_o[`VDATA_O_CO_SLICE] <= {R_sl,G_sl,B_sl};
+    else  // not a scanline
+      vdata_o[`VDATA_O_CO_SLICE] <= {R_pp[4],R_pp[4][color_width_i-1],
+                                     G_pp[4],G_pp[4][color_width_i-1],
+                                     B_pp[4],B_pp[4][color_width_i-1]};
 
     // use standard input if no line-doubling
-    if (nENABLE_linemult) begin
-      S_o <= vdata_i[`VDATA_I_SY_SLICE];
-      R_o <= {R_i,R_i[color_width_i-1]};
-      G_o <= {G_i,G_i[color_width_i-1]};
-      B_o <= {B_i,B_i[color_width_i-1]};
-    end
+    if (nENABLE_linemult)
+      vdata_o <= {vdata_i[`VDATA_I_SY_SLICE],
+                  vdata_i[`VDATA_I_RE_SLICE],vdata_i[3*color_width_i-1],
+                  vdata_i[`VDATA_I_GR_SLICE],vdata_i[2*color_width_i-1],
+                  vdata_i[`VDATA_I_BL_SLICE],vdata_i[color_width_i-1]};
   end
 
-
-// post-assignment
-assign vdata_o = {S_o,R_o,G_o,B_o};
 
 endmodule 
